cf6839c6ed6a3d7f13e94092ef416dc76037e0df
vard <vardghaldunts@gmail.com>
2024-11-10 21:47:37 +0400

    done

---
+++ Demo/tree/cmake-build-debug/CMakeFiles/tree2Demo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tree2Demo.
+# This may be replaced when dependencies are built.
---
+++ Demo/tree/main2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<stack>
+class TreeNode {
+public:
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode* parent;
+    int data;
+
+    TreeNode(int d= 0 , TreeNode* p=nullptr  ,TreeNode* l=nullptr,TreeNode* r =nullptr)
+        : data(d), parent(p), left(l), right(r) {}
+};
+    void task2( TreeNode* root) {
+
+
+
+        if(root!=nullptr){
+
+
+            task2(root->parent);
+            task2(root->left);
+            task2(root->right);
+
+            std::cout<<root->data<<" ";
+        }
+    }
+
+int main() {
+
+    TreeNode* root = new TreeNode(1);//a
+    root->left = new TreeNode(2);//b
+    root->right = new TreeNode(3);//c
+    root->left->left = new TreeNode(4);//d
+    root->right->left = new TreeNode(5);//e
+    root->right->right = new TreeNode(6);//f
+    root->right->left->right = new TreeNode(7);//g
+    root->right->right->left = new TreeNode(8);//h
+    root->right->right->right= new TreeNode(9);//i
+
+    std::cout << "Task 2: ";
+    task2(root);
+    std::cout << std::endl;
+
+
+    return 0;
+}
